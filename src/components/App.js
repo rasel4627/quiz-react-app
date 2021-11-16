d7053e78e7b705020928fa9e6004160131a36482
Md.Saifur Rahman Rasel <saifurrahmanrasel4627@gmail.com>
2021-11-16 09:46:12 +0600

    Result Page added

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import Layout from "./Layout";
 // import Home from "./pages/Home";
 // import SignUp from "./pages/SignUp";
 // import Login from "./pages/Login";
-import Quiz from "./pages/Quiz";
+// import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 function App() {
   return (
     <div className="App">
@@ -11,7 +12,8 @@ function App() {
         {/* <Home /> */}
         {/* <SignUp /> */}
         {/* <Login /> */}
-        <Quiz />
+        {/* <Quiz /> */}
+        <Result />
       </Layout>
     </div>
   );
