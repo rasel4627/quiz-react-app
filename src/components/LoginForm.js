0df0ea11fce9682dd8f4941619a62957619f92d3
Md.Saifur Rahman Rasel <saifurrahmanrasel4627@gmail.com>
2021-11-21 10:24:52 +0600

    Firebase Authentication added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,82 +1,53 @@
 import React, { useState } from "react";
 import Form from "./Form";
 import TextInput from "./TextInput";
-import CheckBox from "./CheckBox";
 import Button from "./Button";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-export default function SignUpForm() {
-  const [userName, setUserName] = useState("");
+export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [agree, setAgree] = useState("");
 
   const [error, setError] = useState();
   const [loading, setLoading] = useState();
 
-  const { signup } = useAuth();
+  const { login } = useAuth();
   const history = useHistory();
 
   async function handleSubmit(e) {
     e.preventDefault();
-    if (password !== confirmPassword) {
-      return setError("Password don't match!");
-    }
 
     try {
       setError("");
       setLoading(true);
-      await signup(email, password, userName);
+      await login(email, password);
       history.push("/");
     } catch (err) {
       console.log(err);
       setLoading(false);
-      setError("Failed to create an account!");
+      setError("Failed to login!");
     }
   }
-
   return (
-    <Form style={{ height: "500px" }} onSubmit={handleSubmit}>
-      <TextInput
-        type="text"
-        required
-        placeholder="Enter Name"
-        icon="person"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      />
+    <Form style={{ height: "330px" }} onSubmit={handleSubmit}>
       <TextInput
         type="email"
-        required
         placeholder="Enter Email"
         icon="alternate_email"
+        required
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <TextInput
         type="password"
-        required
         placeholder="Enter Password"
         icon="lock"
+        required
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <TextInput
-        type="password"
-        required
-        placeholder="Confirm Password"
-        icon="lock_clock"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <CheckBox
-        required
-        text="I agree to the Terms &amp; Conditions"
-        value={agree}
-        onChange={(e) => setAgree(e.target.value)}
-      />
+
       <Button disabled={loading} type="submit">
         <span>Submit Now</span>
       </Button>
@@ -84,7 +55,7 @@ export default function SignUpForm() {
       {error && <p className="error">{error}</p>}
 
       <div className="info">
-        Already have an account? <a href="login.html">Login</a> instead.
+        Don't have an account? <a href="signup.html">Signup</a> instead.
       </div>
     </Form>
   );
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,29 +1,12 @@
 import Illustration from "../Illustration";
-import Form from "../Form";
-import TextInput from "../TextInput";
-import Button from "../Button";
-import classes from "../../styles/Login.module.css";
+import LoginForm from "../LoginForm";
 export default function Login() {
   return (
     <>
       <h1>Login to your account</h1>
       <div className="column">
         <Illustration />
-        <Form className={`${classes.login}`}>
-          <TextInput
-            type="email"
-            placeholder="Enter Email"
-            icon="alternate_email"
-          />
-          <TextInput type="password" placeholder="Enter Password" icon="lock" />
-
-          <Button>
-            <span>Submit Now</span>
-          </Button>
-          <div className="info">
-            Don't have an account? <a href="signup.html">Signup</a> instead.
-          </div>
-        </Form>
+        <LoginForm />
       </div>
     </>
   );
